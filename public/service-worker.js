5d16e1ad403da9460d3ae4e1c737921c07d1315d
Noah <mcbeasty11.nh@icloud.com>
2021-06-21 09:44:51 -0500

    fixed service worker path

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,15 @@
 const FILES_TO_CACHE = [
-  './index.html',
-  './js/idb.js',
-  './js/index.js',
-  './css/styles.css',
-  './icons/icon-72x72.png',
-  './icons/icon-96x96.png',
-  './icons/icon-128x128.png',
-  './icons/icon-144x144.png',
-  './icons/icon-192x192.png',
-  './icons/icon-384x384.png',
-  './icons/icon-512x512.png',
+  '/index.html',
+  '/js/idb.js',
+  '/js/index.js',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
 ];
 
 // global reference for cache
